8d777bee807cab7292784fb050067660a19b31df
dev-fa <franco.a.dev@proton.me>
2022-08-30 19:17:11 +0800

    Create game function

--- script.js
+++ script.js
@@ -15,42 +15,82 @@ function getComputerChoice() {
     }
 }
 
-let computerChoice = getComputerChoice();
-
-console.log("Computer's choice is " + computerChoice);
-
 // Create a function that plays one round that takes the player and computers input as parameters.
 // Round function should return a string that declares the winner. Example: "You lose! Paper beats rock."
 // Players input should be case insensitive.
 function playRound(playerChoice, computerChoice) {
-    let gameStateMessage;
-    playerChoice = playerChoice.toLowerCase();
+    let gameState;
 
-    
-    if (playerChoice === computerChoice) {
-        return gameStateMessage = "It's a tie!";
-    } else if (playerChoice === "rock" && computerChoice === "paper") {
-        return gameStateMessage = "You lose! Paper beats rock.";
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        return gameStateMessage = "You win! Rock beats scissors.";
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-        return gameStateMessage = "You win! Paper beats rock.";
-    } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        return gameStateMessage = "You lose! Scissors beats paper.";
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        return gameStateMessage = "You win! Scissors beat paper.";
-    } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        return gameStateMessage = "You lose! Rock beats scissors.";
+    if (playerChoice === null) {
+        console.log("Cancelled the round.");
     } else {
-        return gameStateMessage = "Invalid input. Please choose type either 'rock', 'paper', or 'scissors'."
+        if (playerChoice.toLowerCase() === computerChoice) {
+            console.log(`It's a tie! Both players chose ${playerChoice}`);
+            return gameState = "tie";
+        } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "paper") {
+            console.log("You lose! Paper beats rock.");
+            return gameState = "lose";
+        } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+            console.log("You win! Rock beats scissors.");
+            return gameState = "win";
+        } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+            console.log("You win! Paper beats rock.");
+            return gameState = "win";
+        } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
+            console.log("You lose! Scissors beats paper.");
+            return gameState = "lose";
+        } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+            console.log("You win! Scissors beat paper.");
+            return gameState = "win";
+        } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
+            console.log("You lose! Rock beats scissors.");
+            return gameState = "lose";
+        } else {
+            console.log("Invalid input. Please chose either 'rock', 'paper', or 'scissors'. (No spaces)")
+        }
     }
 }
 
-let playerChoice = prompt("Rock, paper, or scissors?");
-console.log("Your choice is " + playerChoice);
+// Create a game function that calls the round function. It plays up to five rounds and keeps score. Prints the game state at the end.
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerChoice;
+    let computerChoice;
+    let gameState;
 
-console.log(playRound(playerChoice, computerChoice));
+    for (i = 0; i < 5; i++) {
+        playerChoice = prompt("Rock, paper, or scissors?");
+        computerChoice = getComputerChoice();
+
+        gameState = playRound(playerChoice, computerChoice);
+
+        if (gameState === "win") {
+            playerScore++;
+            console.log("Player score: " + playerScore);
+            console.log("Computer score: " + computerScore);
+        } else if (gameState === "lose") {
+            computerScore++;
+            console.log("Player score: " + playerScore);
+            console.log("Computer score: " + computerScore);
+        } else if (gameState === "tie") {
+            playerScore++;
+            computerScore++;
+            console.log("Player score: " + playerScore);
+            console.log("Computer score: " + computerScore);
+        }
+    }
 
 
+    if (playerScore > computerScore) {
+        console.log("Player has won the game!")
+    } else if (playerScore < computerScore) {
+        console.log("Computer has won the game!")
+    } else if (playerScore === 0 && computerScore === 0) {
+        console.log("Did you even play?")
+    } else if (playerScore === computerScore) {
+        console.log("The game is tied!")
+    }
+}
 
-// Create a game function that calls the round function. It players up to five rounds and keeps score. Reports the winner or loser at the end.
+game();
