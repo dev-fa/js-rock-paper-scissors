688bd15fadcd3b120aea6bcbe547d16f364d32f5
dev-fa <franco.a.dev@proton.me>
2022-09-05 19:28:13 +0800

    Create basic user interface with no styling
    Creates a basic user interface with no css styling. Modified functions
    such as 'playRound' to accomodate the UI. Also created the functions
    'displayScore', which displays the game score on the DOM, and
    'displayWinner, which displays the winner of the game on the DOM.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// Create a function the returns either rock, paper, or scissors randomly and use it as the computers input.
+var playerScore = 0;
+var computerScore = 0;
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+rockBtn.addEventListener("click", playRound);
+paperBtn.addEventListener("click", playRound);
+scissorsBtn.addEventListener("click", playRound);
+
+// Returns either rock, paper, or scissors randomly and use it as the computers input.
 function getComputerChoice() {
     let computerChoice;
 
@@ -9,7 +18,7 @@ function getComputerChoice() {
         case 2:
             return computerChoice = "paper";
             break;
-        case 3: 
+        case 3:
             return computerChoice = "scissors";
             break;
     }
@@ -18,79 +27,110 @@ function getComputerChoice() {
 // Create a function that plays one round that takes the player and computers input as parameters.
 // Round function should return a string that declares the winner. Example: "You lose! Paper beats rock."
 // Players input should be case insensitive.
-function playRound(playerChoice, computerChoice) {
-    let gameState;
+function playRound(e) {
 
-    if (playerChoice === null) {
-        console.log("Cancelled the round.");
-    } else {
-        if (playerChoice.toLowerCase() === computerChoice) {
-            console.log(`It's a tie! Both players chose ${playerChoice}`);
-            return gameState = "tie";
-        } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "paper") {
-            console.log("You lose! Paper beats rock.");
-            return gameState = "lose";
-        } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
-            console.log("You win! Rock beats scissors.");
-            return gameState = "win";
-        } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "rock") {
-            console.log("You win! Paper beats rock.");
-            return gameState = "win";
-        } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
-            console.log("You lose! Scissors beats paper.");
-            return gameState = "lose";
-        } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
-            console.log("You win! Scissors beat paper.");
-            return gameState = "win";
-        } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
-            console.log("You lose! Rock beats scissors.");
-            return gameState = "lose";
-        } else {
-            console.log("Invalid input. Please chose either 'rock', 'paper', or 'scissors'. (No spaces)")
+    let playerChoice = this.classList.value;
+    let computerChoice = getComputerChoice();
+
+    const gameState = document.querySelector(".game-state");
+    if (gameState.hasChildNodes()) {
+        while (gameState.firstChild) {
+            gameState.removeChild(gameState.firstChild);
         }
     }
+    const gameStateMsg = document.createElement("p");
+    gameStateMsg.setAttribute("id", "game-state-msg");
+
+    // Tie.
+    if (playerChoice.toLowerCase() === computerChoice) {
+        gameStateMsg.textContent = `It's a tie! Both players chose ${playerChoice}`
+        playerScore++;
+        computerScore++;
+        displayScore(playerScore, computerScore);
+
+    // Lose. Rock < Paper
+    } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "paper") {
+        gameStateMsg.textContent = "You lose! Paper beats rock.";
+        computerScore++;
+        displayScore(playerScore, computerScore);
+
+    // Win. Rock > Scissors
+    } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+        gameStateMsg.textContent = "You win! Rock beats scissors.";
+        playerScore++;
+        displayScore(playerScore, computerScore);
+
+    // Win. Paper > Rock
+    } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+        gameStateMsg.textContent = "You win! Paper beats rock.";
+        playerScore++;
+        displayScore(playerScore, computerScore);
+
+    // Lose. Paper < Scissors
+    } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
+        gameStateMsg.textContent = "You lose! Scissors beats paper.";
+        computerScore++;
+        displayScore(playerScore, computerScore);
+
+    // Win. Scissors > Paper
+    } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+        gameStateMsg.textContent = "You win! Scissors beat paper.";
+        playerScore++;
+        displayScore(playerScore, computerScore);
+
+    // Lose. Scissors < Rock
+    } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
+        gameStateMsg.textContent = "You lose! Rock beats scissors.";
+        computerScore++;
+        displayScore(playerScore, computerScore);
+    }
+
+    gameState.appendChild(gameStateMsg);
 }
 
-// Create a game function that calls the round function. It plays up to five rounds and keeps score. Prints the game state at the end.
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let playerChoice;
-    let computerChoice;
-    let gameState;
+// Display the running score
+function displayScore(playerScore, computerScore) {
+    const scoreBoard = document.querySelector(".score-board");
+    if (scoreBoard.hasChildNodes()) {
+        while (scoreBoard.firstChild) {
+            scoreBoard.removeChild(scoreBoard.firstChild);
+        }
+    }
+    const pScore = document.createElement("p");
+    pScore.textContent = `Player Score: ${playerScore}`;
+    scoreBoard.appendChild(pScore);
+    const cScore = document.createElement("p");
+    cScore.textContent = `Computer Score: ${computerScore}`;
+    scoreBoard.appendChild(cScore);
 
-    for (i = 0; i < 5; i++) {
-        playerChoice = prompt("Rock, paper, or scissors?");
-        computerChoice = getComputerChoice();
+    // Announce winner once a computer or player reaches 5 points
+    if (playerScore === 5 && computerScore === 5) {
+        displayWinner("tie");
+    } else if (playerScore === 5) {
+        displayWinner("player");
+    } else if (computerScore === 5) {
+        displayWinner("computer");
+    }
+}
+displayScore(playerScore, computerScore);
 
-        gameState = playRound(playerChoice, computerChoice);
+function displayWinner(winner) {
+    const winnerContainer = document.querySelector(".winner")
+    const winnerMsg = document.createElement("p");
 
-        if (gameState === "win") {
-            playerScore++;
-            console.log("Player score: " + playerScore);
-            console.log("Computer score: " + computerScore);
-        } else if (gameState === "lose") {
-            computerScore++;
-            console.log("Player score: " + playerScore);
-            console.log("Computer score: " + computerScore);
-        } else if (gameState === "tie") {
-            playerScore++;
-            computerScore++;
-            console.log("Player score: " + playerScore);
-            console.log("Computer score: " + computerScore);
+    if (winnerContainer.hasChildNodes()) {
+        while (winnerContainer.firstChild) {
+            winnerContainer.removeChild(winnerContainer.firstChild);
         }
     }
 
-
-    if (playerScore > computerScore) {
-        console.log("Player has won the game!")
-    } else if (playerScore < computerScore) {
-        console.log("Computer has won the game!")
-    } else if (playerScore === 0 && computerScore === 0) {
-        console.log("Did you even play?")
-    } else if (playerScore === computerScore) {
-        console.log("The game is tied!")
+    if (winner === "tie") {
+        winnerMsg.textContent = "The game is tied!"
+    } else if (winner === "player") {
+        winnerMsg.textContent = "The player wins!"
+    } else if (winner === "computer") {
+        winnerMsg.textContent = "The computer wins!"
     }
-}
 
-game();
+    winnerContainer.appendChild(winnerMsg);
+}
